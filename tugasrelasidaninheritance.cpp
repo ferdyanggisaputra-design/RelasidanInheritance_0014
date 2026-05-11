78eb6b0a40c961e3735e0bbceced36546de6fd51
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-05-11 13:11:36 +0700

    menambahkan konstruktor dan fungsi generateId pada kelas User

--- tugasrelasidaninheritance.cpp
+++ tugasrelasidaninheritance.cpp
@@ -12,3 +12,17 @@ protected:
     string nama;
     string email;
 
+public:
+    User(string pNama, string pEmail) : nama(pNama), email(pEmail) {
+        id = generateId();
+    }
+
+    // Member function untuk generate ID secara otomatis
+    int generateId() {
+        return ++globalId;
+    }
+
+    // Getter untuk kebutuhan display
+    int getId() { return id; }
+    string getNama() { return nama; }
+};
